7c6296fb0a5bc1ce68fdd61cc65ba24fea040f3c
Jaremy Creechley <creechley@gmail.com>
2017-06-07 14:55:55 -0600

    New app

---
+++ lib/couchdb_mixapp.ex
@@ -0,0 +1,18 @@
+defmodule CouchdbMixapp do
+  @moduledoc """
+  Documentation for CouchdbMixapp.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CouchdbMixapp.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/couchdb_mixapp_test.exs
@@ -0,0 +1,8 @@
+defmodule CouchdbMixappTest do
+  use ExUnit.Case
+  doctest CouchdbMixapp
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
