14bc959f182b7343275a30e3a61a45e89a925f83
Jaremy Creechley <creechley@gmail.com>
2017-06-13 11:40:58 -0600

    mix version updates

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule CouchdbMixapp.Mixfile do
 
   def project do
     [app: :couchdb_mixapp,
-     version: "0.1.0",
+     version: "0.2.0",
      elixir: "~> 1.4",
     #  compilers: [:elixir_make] ++ Mix.compilers,
     #  make_clean: ["clean"],
